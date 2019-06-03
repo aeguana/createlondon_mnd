341bf089bfb81d8f52783cb32c85e490a21d077c
Vieira <fabiovieira79@hotmail.com>
2019-06-03 11:37:12 +0100

    add videos in details capture page

--- vendingapp/static/vendingapp/states/collect-data/details/details-capture.js
+++ vendingapp/static/vendingapp/states/collect-data/details/details-capture.js
@@ -4,8 +4,10 @@ app.controller('EmailCaptureCtrl', function (
     $state,
     $scope,
     dataCaptureService,
-    vendService
+    vendService,
+    $timeout
 ) {
+
     var listenerEvent = $scope.$on('ALERT', function (event, data) {
         if (data == 69) {
             $state.go('root.data');
@@ -14,13 +16,38 @@ app.controller('EmailCaptureCtrl', function (
         }
     });
 
+    $timeout(function () {
+        $scope.refreshVideo(2);
+    }, 10000);
+
+    $scope.refreshVideo = function (status) {
+        var parent = document.getElementById('video-container');
+        parent.innerHTML = "";
+        var video = document.createElement('video');
+        parent.appendChild(video);
+        if (status == 1) {
+            video.src = '/static/vendingapp/assets/videos/loop.mp4';
+            video.loop = true;
+            video.play();
+        } else {
+            video.src = '/static/vendingapp/assets/videos/video.mp4';
+            video.onended = function (e) {
+                $scope.refreshVideo(1);
+            };
+            video.play();
+        }
+    };
+
+    $scope.activeVideo = function () {
+        $scope.refreshVideo(1);
+    };
+
+    $scope.insertPin = function () {
+        $state.go('root.data');
+    };
+
     //Clean up
     $scope.$on('$destroy', function () {
         listenerEvent();
     });
-
-
-    $scope.insertPin = function () {
-        $state.go('root.data');
-    }
 });
